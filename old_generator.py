00dd6d360fea3ae281708333c6e71e5435dc037d
Abilityguy <33577829+Abilityguy@users.noreply.github.com>
2020-05-03 22:56:17 +0530

    Minor change

--- old_generator.py
+++ old_generator.py
@@ -71,7 +71,7 @@ class DataGenerator():
     def __init__(self, heirarchy, path, filename, maxlen):
         self.maxlen = maxlen
         self.path = path
-        self.list_npy_fname(path, heirarchy)
+        self.list_npy_fname(path, heirarchy, filename)
         self.progress = 0
         self.n_samples = self.X.shape[0]
     def get_next_batch(self, batch_size):
